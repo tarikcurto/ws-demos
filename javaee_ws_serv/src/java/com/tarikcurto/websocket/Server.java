43fc348c86d06148dcce6cccb6ac811cdf63a30b
DESKTOP-ST71QLT\tcurto <tcurto@opensistemas.com>
2017-10-25 15:43:34 +0200

    + Java logger

--- javaee_ws_serv/src/java/com/tarikcurto/websocket/Server.java
+++ javaee_ws_serv/src/java/com/tarikcurto/websocket/Server.java
@@ -1,6 +1,8 @@
 package com.tarikcurto.websocket;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.enterprise.context.ApplicationScoped;
@@ -19,14 +21,21 @@ import javax.websocket.server.ServerEndpoint;
 @ServerEndpoint("/ws")
 public class Server {
 
+    /**
+     *
+     */
+    protected final Logger logger = Logger.getLogger( Server.class.getName() );
+    
     @OnOpen
     public void open(Session session) {
         
+        logger.log(Level.INFO, "Conected user_id: {0}", session.getId());
     }
 
     @OnClose
     public void close(Session session) {
         
+        logger.log(Level.INFO, "Disconnected user_id: {0}", session.getId());
     }
 
     @OnError
@@ -36,6 +45,8 @@ public class Server {
 
     @OnMessage
     public void handleMessage(String message, Session session) {
+        
+        logger.log(Level.INFO, "Message user_id: {0}", session.getId());
         
         try {
             session.getBasicRemote().sendText("Client message is: " + message);
